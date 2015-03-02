876f622530c21da8f56e07a1f7f1fac606aedb7f
simmslevy <simmslevy@gmail.com>
2015-03-02 17:26:04 -0500

    aarms

--- Suspensionkinematics/suspension_maker.m
+++ Suspensionkinematics/suspension_maker.m
@@ -1,5 +1,17 @@
-function [ tree ] = suspension_maker( P, plo )
-% P is 5 x 2 that represnts positions of key points in ride (y,z)
+function [ tree ] = suspension_maker( P, rod_type, plo )
+% P is 4 x 2 that represnts positions of key points in ride (y,z) for aarms
+tree.uoarm = P(1,:);        % Upper outer A-arm
+tree.loarm = P(2,:);        % Lower outer A-arm
+tree.uiarm = P(3,:);        % Upper inner A-arm
+tree.liarm = P(4,:);        % Lower inner A-arm
+if rod_type == 'Pullrod'
+    tree.prodo = tree.uoarm-(1.5,1.5);        % Prod outboard point
+elseif rod_type = 'Bottom Pushrod'
+     tree.prodo = tree.loarm-(1.5,-1.5);
+elseif rod_type = 'Top Pushrod'
+    tree.prodo = tree.uoarm-(1.5,-1.5);
+end
+% TODO: make P the right size
 
 % TODO: configure so any damper length can be given and function returns
 % ride config
@@ -14,9 +26,11 @@ function [ tree ] = suspension_maker( P, plo )
 
 % TODO: Check faults
 
+% TODO: Adjust bellcranks
+
 % Intialize suspension
-tree.prodo = P(1,:);        % Pullrod outboard point
-tree.prodi = P(2,:);        % Pullrod inboard point
+tree.prodo = P(1,:);        % Prod outboard point
+tree.prodi = P(2,:);        % Prod inboard point
 tree.pivot = P(3,:);        % BLC pivot point
 tree.dampb = P(4,:);        % Damper to bellcrank point
 tree.dampc = P(5,:);        % Damper to chassis point
